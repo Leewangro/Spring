9a91f6cd950a3219085552c99df76382ef7e6260
user <dldhkdfh1@gmail.com>
2019-04-23 20:56:01 +0900

    setter 인젝션 기본

--- BoardWeb/src/main/java/com/springbook/biz/SamsungTV.java
+++ BoardWeb/src/main/java/com/springbook/biz/SamsungTV.java
@@ -1,5 +1,41 @@
 package com.springbook.biz;
 
+public class SamsungTV implements TV {
+	private Speaker speaker;
+	private int price;
+	
+	public SamsungTV() {
+		System.out.println("====>삼성티비(1) 객체 생성");
+	}
+	
+	public void setSpeaker(Speaker speaker) {
+		System.out.println("===>setSpeaker() 호출");
+		this.speaker = speaker;
+	}
+
+	public void setPrice(int price) {
+		System.out.println("===>set() 호출");
+		this.price = price;
+	}
+
+	public void powerOn() {
+		System.out.println("삼성티비 ----전원 켠다. (가격 : " + price + ")");
+	}
+
+	public void powerOff() {
+		System.out.println("삼성티비 ----전원 끈다.");
+	}
+
+	public void volumeUp() {
+		speaker.volumeUp();
+	}
+
+	public void volumeDown() {
+		speaker.volumeDown();
+	}
+
+}
+
 /*
  * public class SamsungTV {
  * 
@@ -13,6 +49,8 @@ package com.springbook.biz;
  * 
  * }
  */
+
+
 
 /*
  * public class SamsungTV implements TV { public SamsungTV() {
@@ -30,44 +68,28 @@ package com.springbook.biz;
  */
 
 /*
- * public class SamsungTV { public SamsungTV() {
- * System.out.println("====>삼성티비 객체 생성"); } }
+ * public class SamsungTV implements TV { private Speaker speaker; private int
+ * price;
+ * 
+ * public SamsungTV() { System.out.println("====>삼성티비(1) 객체 생성"); } public
+ * SamsungTV(Speaker speaker) { System.out.println("====>삼성티비(2) 객체 생성");
+ * this.speaker = speaker; }
+ * 
+ * public SamsungTV(Speaker speaker, int price) {
+ * System.out.println("====>삼성티비(3) 객체 생성"); this.speaker = speaker; this.price
+ * = price; }
+ * 
+ * public void powerOn() { System.out.println("삼성티비 ----전원 켠다. (가격 : " + price +
+ * ")"); }
+ * 
+ * public void powerOff() { System.out.println("삼성티비 ----전원 끈다."); }
+ * 
+ * public void volumeUp() { speaker.volumeUp();
+ * System.out.println("삼성티비 ---소리를 올린다"); }
+ * 
+ * public void volumeDown() { speaker.volumeDown();
+ * System.out.println("삼성티비 ----소리를 내린다."); }
+ * 
+ * }
  */
 
-public class SamsungTV implements TV {
-	private Speaker speaker;
-	private int price;
-	
-	public SamsungTV() {
-		System.out.println("====>삼성티비(1) 객체 생성");
-	}
-	public SamsungTV(Speaker speaker) {
-		System.out.println("====>삼성티비(2) 객체 생성");
-		this.speaker = speaker;
-	}
-	
-	public SamsungTV(Speaker speaker, int price) {
-		System.out.println("====>삼성티비(3) 객체 생성");
-		this.speaker = speaker;
-		this.price = price;
-	}
-	
-	public void powerOn() {
-		System.out.println("삼성티비 ----전원 켠다. (가격 : " + price + ")");
-	}
-
-	public void powerOff() {
-		System.out.println("삼성티비 ----전원 끈다.");
-	}
-
-	public void volumeUp() {
-		speaker.volumeUp();
-		System.out.println("삼성티비 ---소리를 올린다");
-	}
-
-	public void volumeDown() {
-		speaker.volumeDown();
-		System.out.println("삼성티비 ----소리를 내린다.");
-	}
-
-}
--- BoardWeb/src/main/java/com/springbook/biz/TVUser.java
+++ BoardWeb/src/main/java/com/springbook/biz/TVUser.java
@@ -25,10 +25,10 @@ public class TVUser {
 		// 2. Spring 컨테이너로부터 필요한 객체를 요청(Lookup)한다.
 		TV tv = (TV) factory.getBean("tv"); // applicationContext에서 id가 tv인 bean 객체를 생성해서 호출할 때 TV타입으로 형변환해서 호출
 		tv.powerOn();
-		tv.powerOff();
 		tv.volumeUp();
 		tv.volumeDown();
-
+		tv.powerOff();
+		
 		// 3 .Spring 컨테이너 종료
 		factory.close();
 
